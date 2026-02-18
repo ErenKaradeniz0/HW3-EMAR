1396db0e5da918096bb6b5f9f978e2d0644ef9d6
Eren Karadeniz <83872752+ErenKaradeniz0@users.noreply.github.com>
2026-02-18 22:39:49 +0300

    Add project files.

---
+++ HW3-EMAR/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HW3_EMAR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kutu Genişliği: ");
+            int.TryParse(Console.ReadLine(), out int width);
+
+            Console.Write("Kutu Yüksekliği: ");
+            int.TryParse(Console.ReadLine(), out int height);
+
+            Console.Write("İçi dolu olacak mı (E/H): ");
+            var fill = Console.ReadLine()?.ToUpper();
+
+            Console.WriteLine();
+
+            // ÜST
+            Console.WriteLine(new string('=', width));
+
+            // ORTA
+            for (int i = 0; i < height - 2; i++)
+            {
+                Console.Write("||");
+
+                if (fill == "E")
+                    Console.Write(new string('*', width - 4));
+                else
+                    Console.Write(new string(' ', width - 4));
+
+                Console.WriteLine("||");
+            }
+
+            // ALT
+            Console.WriteLine(new string('=', width));
+        }
+    }
+}
